b7613547a1672a316cad856a01034c86dced6790
Parth <84613656+parthjadhao@users.noreply.github.com>
2023-10-07 01:34:35 +0530

    Created a dynamic todo website

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function findIndex(arr, id) {
      }
      return -1;
 }
-function removeIndex(arr, index) {
+function removeAtIndex(arr, index) {
      let newTodo = []
      for (let i = 0; i < arr.length; i++) {
           if (i != index) {
@@ -33,33 +33,48 @@ function removeIndex(arr, index) {
      return newTodo;
 }
 
-app.get('/todo', (req, res) => {
-     fs.readFile("todo.json", "utf-8", (err, data) => {
+app.get('/todos', (req, res) => {
+     fs.readFile("todos.json", "utf8", (err, data) => {
           if (err) throw err;
-          res.json(JSON.parse(data))
-     })
-})
+          res.json(JSON.parse(data));
+     });
+});
 
-app.post('/todo', (req, res) => {
+app.post('/todos', (req, res) => {
      const newTodo = {
-          "id": Math.floor(Math.random() * 100000000),
-          "title": req.body.title,
-          "description": req.body.description
-     }
-     todo.push(newTodo)
-     res.status(200).json(todo)
-})
+          id: Math.floor(Math.random() * 1000000), // unique random id
+          title: req.body.title,
+          description: req.body.description
+     };
+     fs.readFile("todos.json", "utf8", (err, data) => {
+          if (err) throw err;
+          const todos = JSON.parse(data);
+          todos.push(newTodo);
+          fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+               if (err) throw err;
+               res.status(201).json(newTodo);
+          });
+     });
+});
 
-app.delete('/todo/:id', (req, res) => {
-     const todoIndex = findIndex(todo, parseInt(req.params.id));
-     if (todoIndex === -1) {
-          res.status(400).send();
-     }
-     else {
-          todo = removeIndex(todo, todoIndex);
-          res.status(200).send()
-     }
-})
+app.delete('/todos/:id', (req, res) => {
+
+     fs.readFile("todos.json", "utf8", (err, data) => {
+          if (err) throw err;
+          const todos = JSON.parse(data);
+          const todoIndex = findIndex(todos, parseInt(req.params.id));
+          if (todoIndex === -1) {
+               res.status(404).send();
+          } else {
+               todos = removeAtIndex(todos, todoIndex);
+               fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+                    if (err) throw err;
+                    res.status(200).send();
+               });
+          }
+     });
+});
+
 //[methode-1]solving cors error
 app.get("/", (req, res) => {
      // import path
